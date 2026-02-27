22f55e9a3a8d2200cf90c746c5f8f0e81ab39b51
aipronob <aipronob@gmail.com>
2026-02-27 13:49:59 +0600

    cashout successful

--- script/cashout.js
+++ script/cashout.js
@@ -5,11 +5,11 @@
 const agentNumber = document.getElementById("input-number");
 //1 get the agent number
 //2 get the amount and convert to number
-//3 get current balance
+
 const agentNum = agentNumber.value;
 
 if (agentNum === "01726001234"){
-  alert("Correct agent Number");
+  console.log (agentNum);
  }
 else {
   alert("Invalid agent Number");
@@ -18,11 +18,21 @@ else {
 
 const inputAmount = document.getElementById("input-amount");
 const cashoutAmount = inputAmount.value;
- if (inputAmount < balance) {
-  alert("Valid input");
- }
- else {
-  alert ("Wrong amount")
- }
+console.log (cashoutAmount);
 
-});
+//3 get current balance
+
+  const balance = document.getElementById("balance");
+  const balanceamount = balance.innerText;
+
+  console.log ("Balance", balanceamount);
+
+  //new balance
+
+  const newBalance = Number(balanceamount) - Number(cashoutAmount);
+
+  console.log (newBalance);
+
+
+
+  })
